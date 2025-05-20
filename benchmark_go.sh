e6fdda11f741c857e4b5881e1a132ea24217f138
Alexandr Kobrin <alexk@canva.com>
2025-05-20 16:07:59 +1000

    vibe-fix go benchmark

--- benchmark_go.sh
+++ benchmark_go.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
+
+cat > /tmp/code.go <<EOF
+package main
+
+import (
+    "fmt"
+    "math/big"
+)
+
+func main() {
+    fmt.Println(func(n int64) *big.Int {
+        r := big.NewInt(1)
+        for i := int64(2); i <= n; i++ {
+            r.Mul(r, big.NewInt(i))
+        }
+        return r
+    }(20000))
+}
+EOF
+
 for i in {1..10}; do
   (
     resp=$(curl -s -w "\nRequest $i: HTTP %{http_code} - %{time_total}s\n" --location 'localhost:1450/run' \
---form 'language="go"' \
---form 'code="package main; import("fmt";"math/big"); func main(){ fmt.Println(func(n int64) *big.Int { r := big.NewInt(1); for i := int64(2); i <= n; i++ { r.Mul(r,big.NewInt(i)) }; return r }(20000)) }
-"')
+      --form 'language=go' \
+      --form "code=< /tmp/code.go")
     echo "$resp"
   ) &
 done
